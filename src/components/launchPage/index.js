a0a27420cabec3a43e90e53855ed218c2fa89618
avinash <avinashghosh@aironext.com>
2021-10-23 09:56:23 +0530

    Added and integrated fade component

--- src/components/launchPage/index.js
+++ src/components/launchPage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col } from "react-bootstrap";
 
 import FullSizeRow from "../../common/wrapper/FullSizeRow";
+import Fade from "../../common/wrapper/Fade";
 import Signin from "./Signin";
 
 import classes from "./styles.module.css";
@@ -11,12 +12,16 @@ export default function LaunchPageComponent() {
     return (
         <FullSizeRow>
             <Col lg={6} className={classes.columnCenter}>
-                <h1 className={classes.heading}>
-                    FastClass
-                </h1>
+                <Fade show={true}>
+                    <h1 className={classes.heading}>
+                        FastClass
+                    </h1>
+                </Fade>
             </Col>
             <Col lg={6} className={classes.columnCenter}>
-                <Signin />
+                <Fade show={true}>
+                    <Signin />
+                </Fade>
             </Col>
         </FullSizeRow>
     );
